920304751c517e5f9b341b197035aa0583c6af98
Mohamed Nouri <simonouri38@gmail.com>
2026-09-04 14:07:51 +0100

    chaikin steps

--- Chaikin.java
+++ Chaikin.java
@@ -6,5 +6,23 @@ public class Chaikin {
     }
 
     public static void chaikinStep(Root root) {
+         long now = System.currentTimeMillis();
+        if (now - root.lastTimePointsUpdated < 500) {
+            return;
+        }
+        root.lastTimePointsUpdated = now;
+
+        if (root.animationJaikinStep >= 7) {
+            root.animationJaikinStep = 0;
+            root.points = new ArrayList<>(root.originalPoints);
+            return;
+        }
+
+        if (root.points == null || root.points.isEmpty()) {
+            root.points = new ArrayList<>(root.originalPoints);
+        }
+
+        root.points = chaikin(root.points);
+        root.animationJaikinStep++;
     }
 }
